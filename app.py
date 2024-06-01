d927a8b67bee1e28ce702f2c76918f44bef08a17
“Randino” <randympreston@gmail.com>
2024-06-01 11:50:38 -0400

    Update Flask app to use PORT environment variable

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def download_file(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
